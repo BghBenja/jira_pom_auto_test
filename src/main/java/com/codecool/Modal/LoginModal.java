181abed28d64030a1de002a71ce4fefe6eb73173
BghBenja <bghbenja@gmail.com>
2022-09-28 17:04:22 +0200

    Create TestLogin and LoginModal

--- src/main/java/com/codecool/Modal/LoginModal.java
+++ src/main/java/com/codecool/Modal/LoginModal.java
@@ -1,4 +1,16 @@
 package com.codecool.Modal;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginModal {
+
+    private final WebDriver webDriver;
+
+    public LoginModal(WebDriver webDriver) {
+        this.webDriver = webDriver;
+        PageFactory.initElements(webDriver, this);
+    }
+
+
 }
--- src/test/java/TestLogin/TestLogin.java
+++ src/test/java/TestLogin/TestLogin.java
@@ -1,4 +1,27 @@
 package TestLogin;
 
+import com.codecool.FileReader;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class TestLogin {
+
+    static WebDriver webDriver;
+
+    private static ChromeOptions browserOptions;
+
+    @BeforeAll
+    public void setProperty(){
+        System.setProperty("webdriver.chrome.driver", FileReader.getValueByKey("driver.location"));
+    }
+
+    @BeforeEach
+    public void openNewTab(){
+        browserOptions = new ChromeOptions();
+        browserOptions.addArguments("--incognito");
+        webDriver = new ChromeDriver(browserOptions);
+    }
 }
